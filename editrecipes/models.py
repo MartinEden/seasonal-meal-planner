a7485f86fa8270d7066ce0275bd6be80f2c9bee9
Martin Eden <martin.s.eden@pm.me>
2018-10-17 11:38:58 +0100

    Speed up functions with memoization

--- editrecipes/models.py
+++ editrecipes/models.py
@@ -1,39 +1,44 @@
 from django.db import models
 from django.contrib import admin
 from django.contrib.admin.widgets import FilteredSelectMultiple
+from memoize import mproperty
+
 
 def months_ago(now, then):
     return now - then if now - then > 0 else now - then + 12
+
 
 class Month(models.Model):
     month = models.CharField(max_length=10)
 
     def this_but_not_last(self):
         return set(self.this_months_recipes()) - set(Month.a_months_recipes(
-            self.id-1 if self.id>1 else 12))
+            self.id - 1 if self.id > 1 else 12))
+
     def this_but_not_next(self):
         return set(self.this_months_recipes()) - set(Month.a_months_recipes(
-            self.id+1 if self.id<12 else 1))
+            self.id + 1 if self.id < 12 else 1))
 
     @staticmethod
     def a_months_recipes(month_id):
         for r in Recipe.objects.all():
             if not r.always_available() and Month.objects.get(id=month_id) in \
-                    r.months_in_season():
+                    r.months_in_season:
                 yield r
 
     def this_months_recipes(self):
         for r in Recipe.objects.all():
-            if not r.always_available() and self in r.months_in_season():
+            if not r.always_available() and self in r.months_in_season:
                 yield r
 
     def this_months_always_available_peak_recipes(self):
         for r in Recipe.objects.all():
-            if r.always_available() and self in r.months_in_peak_season():
+            if r.always_available() and self in r.months_in_peak_season:
                 yield r
 
     def __str__(self):
         return self.month
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
@@ -41,29 +46,30 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+
 class DishType(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
-    seasonal = models.ManyToManyField(Month, related_name = "seasonal_month")
-    peak = models.ManyToManyField(Month, related_name = "peak_month",
+    seasonal = models.ManyToManyField(Month, related_name="seasonal_month")
+    peak = models.ManyToManyField(Month, related_name="peak_month",
                                   blank=True)
     tags = models.ManyToManyField(Tag, blank=True)
 
     def in_season(self, month_id):
-        if Month.objects.get(id=month_id) in self.seasonal.all():
-            return True
-        return False
+        return Month.objects.get(id=month_id) in self.seasonal.all()
 
     class Meta:
         ordering = ["name"]
 
     def __str__(self):
         return self.name
+
 
 class SideDish(models.Model):
     name = models.CharField(max_length=200)
@@ -78,6 +84,7 @@ class SideDish(models.Model):
     def __str__(self):
         return self.name
 
+
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=200, blank=True)
@@ -86,43 +93,43 @@ class Recipe(models.Model):
                                  blank=True, null=True, related_name='recipes')
     sidedish = models.ManyToManyField(SideDish, blank=True)
 
+    @mproperty
     def months_in_season(self):
         months = Month.objects.all()
         for i in self.ingredients.all():
-            months = months & i.seasonal.all() #intersection
+            months = months & i.seasonal.all()  # intersection
         return months
 
+    @mproperty
     def months_in_peak_season(self):
         months = Month.objects.all()
         for i in self.ingredients.all():
             months = months & i.seasonal.all()
             if len(i.peak.all()) > 0:
-                months = months & i.peak.all() #intersection
+                months = months & i.peak.all()  # intersection
         return months
 
     def always_available(self):
-        if len(self.months_in_season()) == 12:
+        if len(self.months_in_season) == 12:
             return True
         else:
             return False
 
     def clashing_seasonality(self):
-        if len(self.months_in_season()) == 0:
+        if len(self.months_in_season) == 0:
             return True
         else:
             return False
 
     def in_season(self, month_id):
-        if month_id in [month.id for month in self.months_in_season()]:
-            return True
-        return False
+        return month_id in [month.id for month in self.months_in_season]
 
     def length_time_in_season(self):
-        return len(self.months_in_season())
+        return len(self.months_in_season)
 
     def season_start(self, month_id):
-        return months_ago(month_id, self.months_in_season()[0].id) if \
-            month_id is not None else self.months_in_season()[0].id
+        return months_ago(month_id, self.months_in_season[0].id) if \
+            month_id is not None else self.months_in_season[0].id
 
     def tags(self):
         tags = []
@@ -138,7 +145,7 @@ class Recipe(models.Model):
         ordering = ["name"]
 
 
-#class RecipeIngredientQuantity(models.Model):
+# class RecipeIngredientQuantity(models.Model):
 #    recipe = models.ForeignKey(Recipe)
 #    ingredient = models.ForeignKey(Ingredient)
 
