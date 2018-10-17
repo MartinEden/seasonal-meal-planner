85b930d11e4fccf142210ca2b86feca79064dd04
Martin Eden <martin.s.eden@pm.me>
2018-10-17 10:37:26 +0100

    Add all pages to sidebar

--- editrecipes/urls.py
+++ editrecipes/urls.py
@@ -11,15 +11,15 @@ urlpatterns = [
     path('month', views.month),
     path('year-chart', views.year_chart, name='year_chart'),
     path('category/<str:category>/', views.category, name='category'),
-    path('category', views.category),
+    path('category', views.category, name='all_categories'),
     path('sidedish/<str:sidedish>/<int:month_id>/', views.sidedish,
          name='sidedish'),
     path('sidedish/<str:sidedish>/', views.sidedish, name='sidedish'),
-    path('sidedish', views.sidedish),
+    path('sidedish', views.sidedish, name='all_sidedishes'),
     path('select-recipes/<int:month_id>/', views.select_recipes),
-    path('select-recipes', views.select_recipes),
+    path('select-recipes', views.select_recipes, name='select_recipes'),
     path('shopping-list', views.shopping_list, name='shopping_list'),
     path('search', views.search, name='search'),
     path('search/<str:ingredient>/', views.search, name='search'),
-    path('ingredients', views.ingredients)
+    path('ingredients', views.ingredients, name='ingredients')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
