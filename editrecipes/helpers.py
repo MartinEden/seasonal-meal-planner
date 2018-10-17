3dc0d3bec23f5d5f1cddf77d7a87b342a4ad74ca
Martin Eden <martin.s.eden@pm.me>
2018-10-17 10:07:51 +0100

    Tidy up views

---
+++ editrecipes/helpers.py
@@ -0,0 +1,11 @@
+import datetime
+
+from editrecipes.models import Month
+
+
+def get_month(month_id):
+    if month_id is not None:
+        return Month.objects.get(id=month_id)
+    else:
+        this_month = datetime.datetime.now().strftime("%B")
+        return Month.objects.get(month=this_month)
--- editrecipes/views.py
+++ editrecipes/views.py
@@ -1,22 +1,15 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
+from django.shortcuts import get_object_or_404, render
 
+from editrecipes.helpers import get_month
 from editrecipes.viewmodels import RecipeWithSeasonality
 from .models import Recipe, Month, Tag, DishType, SideDish, Ingredient
-import datetime
-
-
-def get_month(month_id):
-    if month_id is not None:
-        return Month.objects.get(id=month_id)
-    else:
-        this_month = datetime.datetime.now().strftime("%B")
-        return Month.objects.get(month=this_month)
 
 
 def index(request):
     return month(request)
+
 
 def recipe(request, name, month_id=None):
     recipe = get_object_or_404(Recipe, name__exact=name)
@@ -24,35 +17,40 @@ def recipe(request, name, month_id=None):
     dishes = {}
     for dish in recipe.sidedish.all():
         dishes[dish] = dish.seasonal_ingredients(this_month.id)
-    val  = URLValidator()
+    val = URLValidator()
     try:
         val(recipe.url)
         valid_url = True
     except ValidationError:
         valid_url = False
-    return render(request, 'editrecipes/recipe.html', {'recipe': recipe,
-                                                       'months':
-                                                           Month.objects.all(),
-                                                       'this_month':this_month,
-                                                        'dishes':dishes,
-                                                       'valid_url':valid_url})
+
+    data = {
+        'recipe': recipe,
+        'months': Month.objects.all(),
+        'this_month': this_month,
+        'dishes': dishes,
+        'valid_url': valid_url
+    }
+    return render(request, 'editrecipes/recipe.html', data)
+
 
 def grid(request):
     return render(request, 'editrecipes/recipe-month.html',
-                  {'recipes':Recipe.objects.all(),
+                  {'recipes': Recipe.objects.all(),
                    'months': Month.objects.all()})
 
-def month(request, month_id = None):
+
+def month(request, month_id=None):
     month = get_month(month_id)
     recipes = sorted(Recipe.objects.all(), key=lambda x: (-x.in_season(
         month.id), x.length_time_in_season(), x.season_start(month_id)))
 
     seasonal_recipes = dict((r.name, RecipeWithSeasonality(r)) for r in
-                        month.this_months_recipes())
+                            month.this_months_recipes())
     for r in month.this_but_not_last():
         seasonal_recipes[r.name].just_in = True
     for r in month.this_but_not_next():
-            seasonal_recipes[r.name].last_chance = True
+        seasonal_recipes[r.name].last_chance = True
 
     always_available_recipes = month.this_months_always_available_peak_recipes()
     clashing_season_recipes = [r for r in recipes if r.clashing_seasonality()]
@@ -72,36 +70,39 @@ def month(request, month_id = None):
 def category(request, category=None):
     if category is None:
         return render(request, 'editrecipes/categories.html',
-                      {'categories':DishType.objects.all()})
+                      {'categories': DishType.objects.all()})
     else:
         dishtype = get_object_or_404(DishType, name__exact=category)
         return render(request, 'editrecipes/category.html',
-                      {'category':dishtype})
+                      {'category': dishtype})
 
-def sidedish(request, sidedish=None, month_id = None):
+
+def sidedish(request, sidedish=None, month_id=None):
     if sidedish is None:
         return render(request, 'editrecipes/sidedishes.html',
-                      {'sidedishes':SideDish.objects.all()})
+                      {'sidedishes': SideDish.objects.all()})
     else:
         sidedish = get_object_or_404(SideDish, name__exact=sidedish)
         month = get_month(month_id)
         return render(request, 'editrecipes/sidedish.html',
-                      {'sidedish':sidedish,
-                       'seasonal':sidedish.seasonal_ingredients(month.id),
-                       'months':Month.objects.all(),
-                       'this_month':month})
+                      {'sidedish': sidedish,
+                       'seasonal': sidedish.seasonal_ingredients(month.id),
+                       'months': Month.objects.all(),
+                       'this_month': month})
 
-def select_recipes(request, month_id = None):
+
+def select_recipes(request, month_id=None):
     month = get_month(month_id)
     recipes = sorted(Recipe.objects.all(), key=lambda x: (-x.in_season(
         month.id), x.length_time_in_season(), x.season_start(month_id)))
     context = {
         'recipes': recipes,
-         'months': Month.objects.all(),
+        'months': Month.objects.all(),
         'this_month': month,
         'tags': Tag.objects.all()
     }
     return render(request, 'editrecipes/select-recipes.html', context)
+
 
 def shopping_list(request):
     recipe_ids = request.GET.getlist('recipes')
@@ -113,6 +114,7 @@ def shopping_list(request):
     context = {"ingredients": ingredients}
     return render(request, 'editrecipes/shopping-list.html', context)
 
+
 def search(request):
     if 'ingredient' in request.GET:
         search = request.GET['ingredient']
@@ -123,16 +125,17 @@ def search(request):
             for rec in r:
                 print(rec)
         return render(request, 'editrecipes/search.html',
-                      {'recipes':recipes,
-                       'ingredient':search,
+                      {'recipes': recipes,
+                       'ingredient': search,
                        'ingredients': ingredients})
     else:
         return render(request, 'editrecipes/search.html', {})
 
-def ingredients(request, month_id = None):
+
+def ingredients(request, month_id=None):
     month = get_month(month_id)
     return render(request, 'editrecipes/ingredients.html',
-                  {'this_month':month,
-                   'ingredients':Ingredient.objects.all(),
-                   'months':Month.objects.all()
+                  {'this_month': month,
+                   'ingredients': Ingredient.objects.all(),
+                   'months': Month.objects.all()
                    })
